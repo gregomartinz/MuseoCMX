557f5ba8bb2858868e51509ae57774212fa7fdc1
gregomartinz <gregomartinz@gmail.com>
2016-04-27 10:58:29 +0200

    Main Layout working

--- app/src/main/java/cmx/acuntia/es/museocmx/MainActivity.java
+++ app/src/main/java/cmx/acuntia/es/museocmx/MainActivity.java
@@ -10,11 +10,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button botonMap;
-    Button botonZona;
+    ImageButton botonMap;
+    ImageButton botonZona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +24,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        botonMap = (Button) findViewById(R.id.button);
-        botonZona = (Button) findViewById(R.id.button);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        botonMap = (ImageButton) findViewById(R.id.imageButton);
+        botonMap.setImageResource(R.drawable.mapsicon);
+        botonZona = (ImageButton) findViewById(R.id.imageButton2);
+        botonZona.setImageResource(R.drawable.painting);
+
 
         botonMap.setOnClickListener(new View.OnClickListener() {
             @Override
